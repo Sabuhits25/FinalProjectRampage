406cc450defaeaf846122342e55e354d0ac34859
Sabuhits25 <sabuhits@code.edu.az>
2025-06-17 18:19:48 +0400

    BlogRepository ready

---
+++ Rampage.DAL/Repositories/Implementation/BlogTranslationRepository.cs
@@ -0,0 +1,13 @@
+﻿using Rampage.Core.Entities;
+using Rampage.DAL.Context;
+using Rampage.DAL.Repositories.Interfaces;
+
+namespace Rampage.DAL.Repositories.Implementation
+{
+    public class BlogTranslationRepository : Repository<BlogTranslation>, IBlogTranslationRepository
+    {
+        public BlogTranslationRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
--- Rampage/obj/Debug/net8.0/Rampage.AssemblyInfo.cs
+++ Rampage/obj/Debug/net8.0/Rampage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rampage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465fabaedbdfda7181d5f0d534770762c7da02aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a94d36eccfe783529bf943908897e747d594289")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rampage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rampage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
