f6a6d1f9a9568d718d0b483d59c8d8e9fe1103ed
Sabuhits25 <sabuhits@code.edu.az>
2025-06-18 20:31:57 +0400

    CloudinaryService and interfaces ready

---
+++ Rampage.BLL/Services/CloudinaryService.cs
@@ -0,0 +1,38 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Rampage.BLL.Services.Interfaces;
+
+namespace Rampage.BLL.Services
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryService(Cloudinary cloudinary)
+        {
+            _cloudinary = cloudinary;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            using (var stream = file.OpenReadStream())
+            {
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Folder = "uploads",
+                    PublicId = Guid.NewGuid().ToString() + "_" + Path.GetFileNameWithoutExtension(file.FileName),
+                    Overwrite = true
+                };
+
+                var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+                if (uploadResult.StatusCode != System.Net.HttpStatusCode.OK)
+                    throw new Exception("Image upload failed: " + uploadResult.Error?.Message);
+
+                return uploadResult.SecureUrl.ToString();
+            }
+        }
+    }
+}
---
+++ Rampage.BLL/Services/Interfaces/ICloudinaryService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Rampage.BLL.Services.Interfaces
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImageAsync(IFormFile file);
+    }
+}
